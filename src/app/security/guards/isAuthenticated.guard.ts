feba13607baf2793522a8132d5210110888c4e77
Diego Flores <dfloresc1602@alumno.ipn.mx>
2024-11-24 23:26:34 -0600

    fix: HotFix Release Version 1.0.0 - Production

--- src/app/security/guards/isAuthenticated.guard.ts
+++ src/app/security/guards/isAuthenticated.guard.ts
@@ -3,11 +3,11 @@ import { CanActivateFn, Router } from '@angular/router';
 
 export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
-  const token = localStorage.getItem('access_token'); // Valida el token en localStorage
+  const token = sessionStorage.getItem('access_token'); // Valida el token en localStorage
 
   if (!token) {
     // Si no está autenticado, guarda la URL y redirige a login
-    localStorage.setItem('url', state.url);
+    sessionStorage.setItem('url', state.url);
     console.log('Acceso denegado. Redirigiendo a /auth/login');
     router.navigate(['/auth/login']);
     return false;
