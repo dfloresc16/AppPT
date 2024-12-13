856fdb8679cecb8de254b04577ef19a9a99ab733
Diego Flores <dfloresc1602@alumno.ipn.mx>
2024-12-12 20:55:40 -0600

    fix: Arreglando caja de celular

--- src/app/presentation/pages/registerPage/registerPage.component.ts
+++ src/app/presentation/pages/registerPage/registerPage.component.ts
@@ -1,93 +1,48 @@
-import { UserDTO } from './../../../interfaces/UserDTO';
-import { ApiResponse } from './../../../interfaces/ApiResponse';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors, ValidatorFn, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors, ValidatorFn, ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from './../../services/auth.service';
-import Swal from 'sweetalert2';
-import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './registerPage.component.html',
   standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule,
+  imports:[
     ReactiveFormsModule
   ]
 })
 export default class RegisterPageComponent {
   registerForm: FormGroup;
   isLoading = false;
-  showPassword: boolean = false;
-  showConfirmPassword: boolean = false;
 
   constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
-    this.registerForm = this.fb.group(
-      {
-        firstName: ['', Validators.required],
-        lastName: ['', Validators.required],
-        userName: ['', Validators.required],
-        phoneNumber: ['', [Validators.required, this.phoneNumberValidator,Validators.minLength(10),Validators.maxLength(10)]], // Validador actualizado
-        email: ['', [Validators.required, this.emailDomainValidator]],
-        password: ['', [Validators.required, this.passwordStrengthValidator]], // Validación de contraseña actualizada
-        confirmPassword: ['', Validators.required]
-      },
-      { validators: this.passwordsMatchValidator }
-    );
+    this.registerForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      userName: ['', Validators.required],
+      phoneNumber: ['', [Validators.required, this.phoneNumberValidator]],
+      email: ['', [Validators.required, this.emailDomainValidator]],
+      password: ['', [Validators.required]],
+    });
   }
 
-  onSubmit(): void {
-    if (this.registerForm.valid) {
-      this.isLoading = true; // Activa el spinner
-      const formValues = this.registerForm.value;
+  allowOnlyNumbers(event: KeyboardEvent): void {
+    const charCode = event.key.charCodeAt(0);
+    if (charCode < 48 || charCode > 57) {
+      event.preventDefault();
+    }
+  }
 
-      const userData: UserDTO = {
-        userId: 0,
-        name: formValues.firstName,
-        lastName: formValues.lastName,
-        userName: formValues.userName,
-        phoneNumber: formValues.phoneNumber,
-        password: formValues.password,
-        email: formValues.email,
-        pin: ""
-      };
-
-      this.authService.create(userData).subscribe(
-        (response: ApiResponse<UserDTO>) => {
-          this.isLoading = false; // Desactiva el spinner
-          if (response.success) {
-            Swal.fire({
-              icon: 'success',
-              title: 'Usuario creado',
-              text: `El usuario ${response.body.userName} ha sido creado exitosamente.`,
-            }).then(() => {
-              this.router.navigate(['/auth/active']);
-            });
-          } else {
-            Swal.fire({
-              icon: 'error',
-              title: 'Error al crear el usuario',
-              text: 'Ocurrió un error inesperado.',
-            });
-          }
-        },
-        (error) => {
-          this.isLoading = false; // Desactiva el spinner
-          Swal.fire({
-            icon: 'error',
-            title: 'Error al crear el usuario',
-            text: 'Ocurrió un error inesperado.',
-          });
-        }
-      );
+  limitToTenDigits(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.value.length > 10) {
+      input.value = input.value.slice(0, 10);
     }
   }
 
   phoneNumberValidator(control: AbstractControl): ValidationErrors | null {
     const phone = control.value || '';
-    const valid = /^\d{10}$/.test(phone); // Validar 10 dígitos
+    const valid = /^\d{10}$/.test(phone);
     return valid ? null : { phoneNumberInvalid: true };
   }
 
@@ -97,20 +52,12 @@ export default class RegisterPageComponent {
     return valid ? null : { emailDomain: true };
   }
 
-  passwordStrengthValidator(control: AbstractControl): ValidationErrors | null {
-    const password = control.value || '';
-    const hasUpperCase = /[A-Z]/.test(password); // Verifica mayúsculas
-    const hasNumber = /[0-9]/.test(password);    // Verifica números
-    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password); // Verifica caracteres especiales
-    const validLength = password.length >= 8;     // Verifica longitud mínima de 8
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      this.isLoading = true;
+      const formValues = this.registerForm.value;
 
-    const valid = hasUpperCase && hasNumber && hasSpecialChar && validLength;
-    return valid ? null : { passwordStrength: true };
+      // Lógica para enviar los datos al servidor
+    }
   }
-
-  passwordsMatchValidator: ValidatorFn = (group: AbstractControl): ValidationErrors | null => {
-    const password = group.get('password')?.value;
-    const confirmPassword = group.get('confirmPassword')?.value;
-    return password === confirmPassword ? null : { passwordMismatch: true };
-  };
 }
